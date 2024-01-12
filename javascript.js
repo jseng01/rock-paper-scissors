b963a21eb929532b68577e003c5e44e697a72077
Jseng01 <jseng777@gmail.com>
2024-01-11 23:48:22 -0500

    Create a function that will randomly return either rock paper or scissors

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Add a function that will randomly return either rock, paper, or scissors
+function getComputerChoice(){
+    let options = ["rock", "paper", "scissors"];
+    let choice = options[Math.floor(Math.random()*options.length)];
+}
+getComputerChoice()
+// Get an input of either rock, paper, or scissors from the user
+// Add a function that will plays a single round of rock paper scissors using the computer generated choice and the user input
+
+// Add a function that loops the round funtion until a best of five game has been decided
