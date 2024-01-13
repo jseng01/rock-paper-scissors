69b48f42e4550b62460ec34f159c3971563f1261
Jseng01 <jseng777@gmail.com>
2024-01-12 22:07:44 -0500

    Add a function to run a round of the game

--- javascript.js
+++ javascript.js
@@ -1,26 +1,45 @@
-// Add a function that will randomly return either rock, paper, or scissors
 function getComputerChoice(){
-    let options = ["rock", "paper", "scissors"];
+    let options = ["ROCK", "PAPER", "SCISSORS"];
     let choice = options[Math.floor(Math.random()*options.length)];
-}
-getComputerChoice()
-// Get an input of either rock, paper, or scissors from the user
-function getUserChoice(){
-    selection=prompt("Choose: ROCK, PAPER, or, SCISSORS")
+    return choice
 }
 
-function playerSelection(x,y,z){
+// Add a function that will randomly return either rock, paper, or scissors
+function getUserChoice(){
+    selection=prompt("Choose: ROCK, PAPER, or, SCISSORS")
+    return selection
+}
+// Add a function that prompts the user to input either rock paper or scissors
+
+
+function playerSelection(){
     getUserChoice()
     selectionUpperCase= selection.toUpperCase()
+    // Changes user input to upper case
     if (selectionUpperCase === "ROCK" || selectionUpperCase === "PAPER" || selectionUpperCase === "SCISSORS"){
-        return
+        return selectionUpperCase
     }
     else {
         console.log("Please select either ROCK, PAPER, OR SCISSORS")
         getUserChoice()
     }
 }
-playerSelection()
-// Add a function that will plays a single round of rock paper scissors using the computer generated choice and the user input
+// Get an input of one of the options from the user, or have them enter a new input
 
+
+// Add a function that will plays a single round of rock paper scissors using the computer generated choice and the user input
+function gameRound(){
+    let gameChoice = getComputerChoice()
+    let playerChoice = playerSelection()
+    if (playerChoice==="ROCK" && gameChoice==="ROCK"){
+        console.log("It's a tie! Play again!")
+    }
+    else if (playerChoice==="ROCK" && gameChoice==="SCISSORS"){
+             console.log("You win! Rock beats Scissors")
+    }
+    else if (playerChoice==="ROCK" && gameChoice==="PAPER"){
+        console.log("You lose! Paper beats Rock")
+}
+}
+gameRound()
 // Add a function that loops the round funtion until a best of five game has been decided
