47c3385874875b2037344359572b6104d41b7eaa
Jseng01 <jseng777@gmail.com>
2024-01-14 19:38:19 -0500

    Finish the javascript for rock, paper, scissors

--- javascript.js
+++ javascript.js
@@ -23,44 +23,68 @@ function capatalizeUserChoice(){
 // add a function that verifies that the user input was rock, paper, or scissors
 function gameRound(){
     let playerOne = capatalizeUserChoice()
-    console.log(playerOne)
     let NPC = getRandomChoice()
-    console.log(NPC)
-    if (playerOne === "R" && NPC === "R"){
+    if (playerOne === NPC){
          console.log("It's a tie! Play Again")
          gameRound()
     }
     else if (playerOne=== "R" && NPC === "P"){
         console.log("You lose! Rock loses to paper!")
-        return 
+        return "L"
     }
     else if (playerOne=== "R" && NPC === "S"){
         console.log("You win! Rock beats scissors!")
-        return "Win"
-    }
-    else if (playerOne=== "P" && NPC === "P"){
-        console.log("It's a tie! Play Again")
-        gameRound()
+        return "W"
     }
     else if (playerOne=== "P" && NPC === "S"){
         console.log("You lose! Paper loses to scissors!")
-        return 
+        return "L"
     }
     else if (playerOne=== "P" && NPC === "R"){
         console.log("You win! Paper beats rock!")
-        return "Win"
-    }
-    else if (playerOne=== "S" && NPC === "S"){
-        console.log("It's a tie! Play again")
-        gameRound() 
+        return "W"
     }
     else if (playerOne=== "S" && NPC === "R"){
         console.log("You lose! Scissors lose to Rock")
-        return 
+        return "L"
     }
     else if (playerOne=== "S" && NPC === "P"){
         console.log("You win! Scissors beat paper!")
-        return "Win"
+        return "W"
+    }
+    else {
+        gameRound()
     }
 }
-gameRound()
+// add a function containing a series of if else statements, that represent the rules of rock, paper, scissors
+function game(){
+    let winCount = 0
+    let lossCount = 0
+    while (winCount < 3 && lossCount < 3){   
+        let results = gameRound()
+        if (results === "L"){
+            lossCount++
+            console.log("LossCount= " + lossCount)
+        }
+        else {
+            winCount++
+            console.log("WinCount= " + winCount)
+        }
+    }
+    return {winCount, lossCount}
+}
+// add a function that keeps track of how many wins and losses there are
+
+function result(){
+    let finalScore = game()
+    let wins = finalScore.winCount
+    let losses = finalScore.lossCount
+        if(wins === 3){
+            console.log("Congratulations! You won the game "+ wins +" to "+ losses)
+        }
+        else {
+            console.log("Sorry, you lost this time with a score of " + losses + " to " + wins)
+        }     
+    }
+    // add a function that tells you wether you won or lost the game!
+result()
