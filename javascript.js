a46d9421ed5c774963eee58c2261fe18e4f0ea7d
jseng01 <jseng777@gmail.com>
2024-03-04 01:30:11 -0500

    Add buttons to html and add event listeners to js

--- javascript.js
+++ javascript.js
@@ -1,23 +1,28 @@
+let rBtn = document.querySelector('#ROCK');
+rBtn.addEventListener('click', function(){
+capatalizeUserChoice('ROCK');});
+
+let pBtn = document.querySelector('#PAPER');
+pBtn.addEventListener('click', function(){
+capatalizeUserChoice('PAPER'); });
+
+let sBtn = document.querySelector('#SCISSORS');
+sBtn.addEventListener('click', function() {
+    capatalizeUserChoice('SCISSORS'); });
+
+
 function getRandomChoice(){
     let randomOptions = ["R", "P", "S"];
     let RandomChoice = randomOptions[Math.floor(Math.random()*randomOptions.length)];
     return RandomChoice
 }
 // add a function to randomly return either R, P, or S for rock, paper and scissors
-function getUserChoice(){
-    userChoice = prompt("Choose Rock, Paper, or Scissors")
-    return userChoice
-}
-// add a function that prompts the user to input either rock, paper, or scissors
-function capatalizeUserChoice(){
-    getUserChoice()
-    let capitalUserChoice = userChoice.toUpperCase()
+function capatalizeUserChoice(btn){
 // change user input to uppercase
-    if (capitalUserChoice === "ROCK" || capitalUserChoice === "PAPER" || capitalUserChoice === "SCISSORS"){
+    if (btn === "ROCK" || btn === "PAPER" || btn === "SCISSORS"){
         return capitalUserChoice.slice(0,1)}
         else {
             console.log("Please select either ROCK, PAPER, OR SCISSORS")
-            getUserChoice()
         }
 }
 // add a function that verifies that the user input was rock, paper, or scissors
@@ -57,34 +62,37 @@ function gameRound(){
     }
 }
 // add a function containing a series of if else statements, that represent the rules of rock, paper, scissors
-function game(){
-    let winCount = 0
-    let lossCount = 0
-    while (winCount < 3 && lossCount < 3){   
-        let results = gameRound()
-        if (results === "L"){
-            lossCount++
-            console.log("LossCount= " + lossCount)
-        }
-        else {
-            winCount++
-            console.log("WinCount= " + winCount)
-        }
-    }
-    return {winCount, lossCount}
-}
+
+// function game(){
+//     let winCount = 0
+//     let lossCount = 0
+//     while (winCount < 3 && lossCount < 3){   
+//         let results = gameRound()
+//         if (results === "L"){
+//             lossCount++
+//             console.log("LossCount= " + lossCount)
+//         }
+//         else {
+//             winCount++
+//             console.log("WinCount= " + winCount)
+//         }
+//     }
+//     return {winCount, lossCount}
+// }
 // add a function that keeps track of how many wins and losses there are
 
-function result(){
-    let finalScore = game()
-    let wins = finalScore.winCount
-    let losses = finalScore.lossCount
-        if(wins === 3){
-            console.log("Congratulations! You won the game "+ wins +" to "+ losses)
-        }
-        else {
-            console.log("Sorry, you lost this time with a score of " + losses + " to " + wins)
-        }     
-    }
+// function result(){
+//     let finalScore = game()
+//     let wins = finalScore.winCount
+//     let losses = finalScore.lossCount
+//         if(wins === 3){
+//             console.log("Congratulations! You won the game "+ wins +" to "+ losses)
+//         }
+//         else {
+//             console.log("Sorry, you lost this time with a score of " + losses + " to " + wins)
+//         }     
+//     }
     // add a function that tells you wether you won or lost the game!
-result()
+// result()
+
+// rps ui additions
